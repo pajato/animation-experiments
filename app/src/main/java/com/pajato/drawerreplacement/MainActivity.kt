f5ca5dbdc640e563ef9eb654cbb17fddcca878b2
support <bryan@pajato.com>
2018-02-11 16:45:40 -0500

    Altering animation for the chevrons so they are symmetrical (using play button as the point of reflection) at all stages of the animation.

--- app/src/main/java/com/pajato/drawerreplacement/MainActivity.kt
+++ app/src/main/java/com/pajato/drawerreplacement/MainActivity.kt
@@ -2,7 +2,9 @@ package com.pajato.drawerreplacement
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
+import android.util.TypedValue
 import android.view.View
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import kotlinx.android.synthetic.main.activity_slide.*
@@ -12,8 +14,18 @@ class MainActivity : AppCompatActivity(), SlidingUpPanelLayout.PanelSlideListene
         val cs = ConstraintSet()
         cs.clone(mainPanel)
         cs.setVerticalBias(R.id.playButtonMain, slideOffset * 0.7f)
-        cs.setHorizontalBias(R.id.playButtonMain, (1.0f - slideOffset / 2))
+        cs.setHorizontalBias(R.id.playButtonMain, (1.0f - slideOffset * 0.5f))
         cs.applyTo(mainPanel)
+
+        previousMain.alpha = slideOffset
+        val marginEnd = previousMain.layoutParams as ConstraintLayout.LayoutParams
+        marginEnd.rightMargin = dpToPx(50 * slideOffset)
+        previousMain.layoutParams = marginEnd
+
+        nextMain.alpha = slideOffset
+        val marginStart = nextMain.layoutParams as ConstraintLayout.LayoutParams
+        marginStart.leftMargin = dpToPx(50 * slideOffset)
+        nextMain.layoutParams = marginStart
 
         chevronClick.rotation = 270 - (180 * slideOffset)
     }
@@ -32,4 +44,7 @@ class MainActivity : AppCompatActivity(), SlidingUpPanelLayout.PanelSlideListene
         slidingPanel.addPanelSlideListener(this)
     }
 
+    private fun dpToPx(dp: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics).toInt()
+    }
 }

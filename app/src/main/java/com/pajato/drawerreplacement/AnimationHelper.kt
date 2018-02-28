37e7507913e56c309fc52a17a8cde02d97cbe80c
support <bryan@pajato.com>
2018-02-28 14:34:02 -0500

    Adding a third animation option that deals with bitmap-based animation for the title text view. Removed animation for the duration text as it was deemed not necessary.

--- app/src/main/java/com/pajato/drawerreplacement/AnimationHelper.kt
+++ app/src/main/java/com/pajato/drawerreplacement/AnimationHelper.kt
@@ -1,9 +1,11 @@
 package com.pajato.drawerreplacement
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 import android.util.TypedValue
+import android.view.View
 import com.pajato.drawerreplacement.Dimens.ALPHA_END
 import com.pajato.drawerreplacement.Dimens.ALPHA_START
 import com.pajato.drawerreplacement.Dimens.CHEVRON_ROTATION_END
@@ -12,7 +14,6 @@ import com.pajato.drawerreplacement.Dimens.CHEVRON_ROTATION_START
 import com.pajato.drawerreplacement.Dimens.DURATION_HORIZONTAL_BIAS_SCALE
 import com.pajato.drawerreplacement.Dimens.DURATION_HORIZONTAL_BIAS_START
 import com.pajato.drawerreplacement.Dimens.DURATION_SIZE_END
-import com.pajato.drawerreplacement.Dimens.DURATION_SIZE_SCALE
 import com.pajato.drawerreplacement.Dimens.DURATION_SIZE_SCALE_COMPACT
 import com.pajato.drawerreplacement.Dimens.DURATION_SIZE_START
 import com.pajato.drawerreplacement.Dimens.PLAY_HORIZONTAL_BIAS_SCALE
@@ -37,6 +38,35 @@ import com.pajato.drawerreplacement.Dimens.TITLE_VERTICAL_BIAS_START
 import kotlinx.android.synthetic.main.content_drawer.*
 
 object AnimationHelper {
+    private lateinit var titleBmpStart: Bitmap
+    private lateinit var titleBmpEnd: Bitmap
+    private var smallHeight = 0
+    private var smallWidth = 0
+    private var largeHeight = 0
+    private var largeWidth = 0
+    private var isClosing = false
+    private var durationConnectedToText = true
+
+    fun setTitleBmps(startBitmap: Bitmap?, endBitmap: Bitmap?) {
+        if (startBitmap != null && endBitmap != null) {
+            this.titleBmpStart = startBitmap
+            this.titleBmpEnd = endBitmap
+
+            isClosing = titleBmpStart.width < titleBmpEnd.width
+
+            if (isClosing) {
+                this.smallHeight = titleBmpStart.height
+                this.smallWidth = titleBmpStart.width
+                this.largeHeight = titleBmpEnd.height
+                this.largeWidth = titleBmpEnd.width
+            } else {
+                this.smallHeight = titleBmpEnd.height
+                this.smallWidth = titleBmpEnd.width
+                this.largeHeight = titleBmpStart.height
+                this.largeWidth = titleBmpStart.width
+            }
+        }
+    }
 
     /**
      * An animation method that splits the text sizing and the shifting and fading of the layouts
@@ -48,14 +78,12 @@ object AnimationHelper {
         if (slideOffset < SPLIT_TEXT_END) {
             val tmpOffset = slideOffset * SPLIT_SCALE
             compactScaleTextViews(tmpOffset, mainActivity)
-            // scaleTextViews(tmpOffset, mainActivity)
         } else {
             mainActivity.songTitle.textSize = TITLE_SIZE_END
             mainActivity.songDuration.textSize = DURATION_SIZE_END
         }
 
-        val soundsTextMain = mainActivity.soundsText
-        val playButtonMain = mainActivity.playButton
+        // Begin sliding views to their appropriate positions once we have entered the correct range
         if (slideOffset > SPLIT_SLIDE_START) {
             val tmpOffset = (slideOffset - SPLIT_SLIDE_START) * SPLIT_SCALE
 
@@ -63,78 +91,95 @@ object AnimationHelper {
             fadeAndSlideNextPrevious(tmpOffset, mainActivity)
             scalePlayButton(slideOffset, mainActivity)
 
-            soundsTextMain.alpha = tmpOffset
-
+            mainActivity.soundsText.alpha = tmpOffset
         } else {
-            val mainPanel = mainActivity.mainPanel
-            val cs = ConstraintSet()
-            cs.clone(mainPanel)
-            // Slide the play button, song duration, and song title into the right place
-            cs.setVerticalBias(R.id.playButton, PLAY_VERTICAL_BIAS_START)
-            cs.setHorizontalBias(R.id.playButton, PLAY_HORIZONTAL_BIAS_START)
-            cs.setVerticalBias(R.id.songTitle, TITLE_VERTICAL_BIAS_START)
-            cs.setHorizontalBias(R.id.songTitle, TITLE_HORIZONTAL_BIAS_START)
-            cs.setHorizontalBias(R.id.songDuration, DURATION_HORIZONTAL_BIAS_START)
-            cs.applyTo(mainPanel)
-
-            // Fade out various views and set the play button's size
-            mainActivity.previous.alpha = ALPHA_START
-            mainActivity.next.alpha = ALPHA_START
-            soundsTextMain.alpha = ALPHA_START
-            val params = playButtonMain.layoutParams
-            val side = dpToPx(PLAY_SIDE_DP_START, mainActivity)
-            params.width = side
-            params.height = side
+            // If we haven't entered the appropriate range, lock non-text views in place.
+            slideViewsUsingConstraintBias(0.0f, mainActivity)
+            fadeAndSlideNextPrevious(0.0f, mainActivity)
+            scalePlayButton(0.0f, mainActivity)
         }
     }
 
     /** The "standard" animate method that causes stuttering in the TextViews due to constant resizing. */
-    fun animate(slideOffset: Float, mainActivity: MainActivity) {
+    fun animate(slideOffset: Float, mainActivity: MainActivity, useBitmap: Boolean = false) {
         slideViewsUsingConstraintBias(slideOffset, mainActivity)
         fadeAndSlideNextPrevious(slideOffset, mainActivity)
+        scalePlayButton(slideOffset, mainActivity)
+        if (useBitmap) {
+            scaleTextViewWithBitmap(slideOffset, mainActivity)
+        } else {
+            scaleTextView(slideOffset, mainActivity)
+        }
 
         mainActivity.soundsText.alpha = slideOffset
-
-        scalePlayButton(slideOffset, mainActivity)
-        // compactScaleTextViews(slideOffset, mainActivity)
-        scaleTextViews(slideOffset, mainActivity)
-
         mainActivity.chevronClick.rotation = CHEVRON_ROTATION_START + (CHEVRON_ROTATION_SCALE * slideOffset)
     }
 
     /** Sets all views to the state they should be in when the drawer is closed. */
     fun drawerOpen(mainActivity: MainActivity) {
+        // Because we use ConstraintLayout.LayoutParams to move Next/Previous, and they are altered
+        // when we apply ConstraintSets, we need to apply Constraints first and do LayoutParams after.
+        if (!durationConnectedToText)
+            connectDuration(mainActivity)
         slideViewsUsingConstraintBias(SLIDE_OFFSET_EXPANDED, mainActivity)
         fadeAndSlideNextPrevious(SLIDE_OFFSET_EXPANDED, mainActivity)
-        mainActivity.soundsText.alpha = ALPHA_END
-
         scalePlayButton(SLIDE_OFFSET_EXPANDED, mainActivity)
-        mainActivity.songDuration.setTextSize(TypedValue.COMPLEX_UNIT_SP, DURATION_SIZE_END)
-        mainActivity.songTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, TITLE_SIZE_END)
 
+        mainActivity.songTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, TITLE_SIZE_END)
+        mainActivity.soundsText.alpha = ALPHA_END
         mainActivity.chevronClick.rotation = CHEVRON_ROTATION_END
+
+        // Ensure the Title TextView is visible and its Bitmap ImageView is invisible so RecyclerView changes update correctly.
+        if (mainActivity.songTitle.visibility == View.INVISIBLE)
+            mainActivity.songTitle.visibility = View.VISIBLE
+        if (mainActivity.songTitleBmp.visibility == View.VISIBLE)
+            mainActivity.songTitleBmp.visibility = View.INVISIBLE
     }
 
     /** Sets all views to the state they should be in when the drawer is open. */
     fun drawerClosed(mainActivity: MainActivity) {
+        // Because we use ConstraintLayout.LayoutParams to move Next/Previous, and they are altered
+        // when we apply ConstraintSets, we need to apply Constraints first and do LayoutParams after.
+        if (!durationConnectedToText)
+            connectDuration(mainActivity)
         slideViewsUsingConstraintBias(SLIDE_OFFSET_COLLAPSED, mainActivity)
         fadeAndSlideNextPrevious(SLIDE_OFFSET_COLLAPSED, mainActivity)
-        mainActivity.soundsText.alpha = ALPHA_START
-
         scalePlayButton(SLIDE_OFFSET_COLLAPSED, mainActivity)
-        mainActivity.songDuration.setTextSize(TypedValue.COMPLEX_UNIT_SP, DURATION_SIZE_START)
-        mainActivity.songTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, TITLE_SIZE_START)
 
+        mainActivity.soundsText.alpha = ALPHA_START
+        mainActivity.songTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, TITLE_SIZE_START)
         mainActivity.chevronClick.rotation = CHEVRON_ROTATION_START
+
+        // Ensure the Title TextView is visible and its Bitmap ImageView is invisible so RecyclerView changes update correctly.
+        if (mainActivity.songTitle.visibility == View.INVISIBLE)
+            mainActivity.songTitle.visibility = View.VISIBLE
+        if (mainActivity.songTitleBmp.visibility == View.VISIBLE)
+            mainActivity.songTitleBmp.visibility = View.INVISIBLE
     }
 
-    /** */
-    private fun scaleTextViews(slideOffset: Float, mainActivity: MainActivity) {
-        val durationSp = DURATION_SIZE_START + (slideOffset * DURATION_SIZE_SCALE)
-        mainActivity.songDuration.setTextSize(TypedValue.COMPLEX_UNIT_SP, durationSp)
-
+    /** Scaling by TextSize causes stuttering, so this method is not recommended to use. */
+    private fun scaleTextView(slideOffset: Float, mainActivity: MainActivity) {
         val titleSp = TITLE_SIZE_START + (slideOffset * TITLE_SIZE_SCALE)
         mainActivity.songTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, titleSp)
+    }
+
+    /** Instead of resizing the text using textSize, we resize bitmap images for a smoother animation. */
+    private fun scaleTextViewWithBitmap(slideOffset: Float, mainActivity: MainActivity) {
+        mainActivity.songTitle.visibility = View.INVISIBLE
+        val songTitleBmp = mainActivity.songTitleBmp
+        songTitleBmp.visibility = View.VISIBLE
+
+        // Use the smaller drawable when close to the "smaller" side of the animation to avoid a "settle" when it is replaced by text.
+        if (isClosing) {
+            songTitleBmp.setImageBitmap(if (slideOffset > 0.8f) this.titleBmpStart else this.titleBmpEnd)
+        } else {
+            songTitleBmp.setImageBitmap(if (slideOffset < 0.2f) this.titleBmpStart else this.titleBmpEnd)
+        }
+
+        songTitleBmp.layoutParams.width = smallWidth + ((largeWidth - smallWidth) * slideOffset).toInt()
+        songTitleBmp.layoutParams.height = smallHeight + ((largeHeight - smallHeight) * slideOffset).toInt()
+        if (durationConnectedToText)
+            connectDuration(mainActivity, true)
     }
 
     /** Increase the songDurationMain and songTitleMain TextViews size. */
@@ -164,11 +209,13 @@ object AnimationHelper {
     private fun slideViewsUsingConstraintBias(slideOffset: Float, mainActivity: MainActivity) {
         val cs = ConstraintSet()
         cs.clone(mainActivity.mainPanel)
-        // Slide the play button, song duration, and song title into the right place
+        // Slide the play button, song duration, song title, and song title bitmap into their places.
         cs.setVerticalBias(R.id.playButton, (PLAY_VERTICAL_BIAS_START + (slideOffset * PLAY_VERTICAL_BIAS_SCALE)))
         cs.setHorizontalBias(R.id.playButton, (PLAY_HORIZONTAL_BIAS_START - (slideOffset * PLAY_HORIZONTAL_BIAS_SCALE)))
         cs.setVerticalBias(R.id.songTitle, (TITLE_VERTICAL_BIAS_START + (slideOffset * TITLE_VERTICAL_BIAS_SCALE)))
         cs.setHorizontalBias(R.id.songTitle, (TITLE_HORIZONTAL_BIAS_START + (slideOffset * TITLE_HORIZONTAL_BIAS_SCALE)))
+        cs.setVerticalBias(R.id.songTitleBmp, (TITLE_VERTICAL_BIAS_START + (slideOffset * TITLE_VERTICAL_BIAS_SCALE)))
+        cs.setHorizontalBias(R.id.songTitleBmp, (TITLE_HORIZONTAL_BIAS_START + (slideOffset * TITLE_HORIZONTAL_BIAS_SCALE)))
         cs.setHorizontalBias(R.id.songDuration, (DURATION_HORIZONTAL_BIAS_START + (slideOffset * DURATION_HORIZONTAL_BIAS_SCALE)))
         cs.applyTo(mainActivity.mainPanel)
     }
@@ -198,6 +245,22 @@ object AnimationHelper {
         val side = dpToPx((PLAY_SIDE_DP_START + (slideOffset * PLAY_SIDE_DP_SCALE)), mainActivity)
         params.width = side
         params.height = side
+    }
+
+    /** Because we use an ImageView for the bitmap animation strategy, we need to change songDuration's
+     * constraints to either the bitmap or the TextView depending on the strategy chosen. */
+    private fun connectDuration(mainActivity: MainActivity, toBmp: Boolean = false) {
+        val cs = ConstraintSet()
+        val connection = if (toBmp) R.id.songTitleBmp else R.id.songTitle
+        this.durationConnectedToText = !toBmp
+        cs.clone(mainActivity.mainPanel)
+        cs.clear(R.id.songDuration, ConstraintSet.TOP)
+        cs.clear(R.id.songDuration, ConstraintSet.END)
+        cs.clear(R.id.songDuration, ConstraintSet.START)
+        cs.connect(R.id.songDuration, ConstraintSet.TOP, connection, ConstraintSet.BOTTOM)
+        cs.connect(R.id.songDuration, ConstraintSet.END, connection, ConstraintSet.END)
+        cs.connect(R.id.songDuration, ConstraintSet.START, connection, ConstraintSet.START)
+        cs.applyTo(mainActivity.mainPanel)
     }
 
     /** Converts a dp measurement (Float) into pixels (Int) */
